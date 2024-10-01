23eaa53e5bc5ec2319e0d242aeb7ee928957738f
cpeel42 <175068649+cpeel42@users.noreply.github.com>
2024-09-30 21:32:48 -0400

    Finish basic game logic

--- scripts.js
+++ scripts.js
@@ -1,84 +1,63 @@
-const game = (function() {
-    const gameBoard = (function() {
-        const create = function() {
-            let spaces = ['', '', '', '', '', '', '', '', ''];
-            return {spaces};
-        }
-        return {create};
-    })();
-    
-    const player = (function() {
-        const create = function(playerName, flip) {
-            const name = playerName;
-            const symbol = flip ? 'x' : 'o';
-            console.log(`Player name is ${name}(${symbol})`)
-            return {name, symbol};
-        }
-        return {create};
-    })();
-
-    const logic = (function() {
-        const create = function() {
-            const echo = function() {
-                console.log(gameBoards.spaces);
-                // console.log(`The players are: ${players[0].echo()} and ${players[1].echo()}`);
-                // console.log(players[0]);
-                // console.log(`The game state is ${gameBoards().spaces}`);
-            }
-            const makePlayers = function() {
-                const playerOne = player.create('playerOne', flip)
-                const playerTwo = player.create('playerTwo', !flip)
-                const players = [playerOne, playerTwo]
-                return players;
-            }
-            const makeGameBoard = function() {
-                return gameBoard.create();
-            }
-            const makeMove = function(player, boardIndexNumber) {
-                gameBoards.spaces[boardIndexNumber] = player.symbol;
-                echo();
-                
-            }
-            const checkForWin = function() {
-                let isMatch = false;
-                function rowOrDiagIsMatch (a,b,c) {
-                    if (a === b && a === c && a != false) {
-                        isMatch = true;
-                    } 
-                }
-                let spaces = gameBoards.spaces;
-                rowOrDiagIsMatch(spaces[0], spaces[1], spaces[2]);
-                rowOrDiagIsMatch(spaces[3], spaces[4], spaces[5]);
-                rowOrDiagIsMatch(spaces[6], spaces[7], spaces[8]);
-                rowOrDiagIsMatch(spaces[0], spaces[3], spaces[6]);
-                rowOrDiagIsMatch(spaces[1], spaces[4], spaces[7]);
-                rowOrDiagIsMatch(spaces[2], spaces[5], spaces[8]);
-                rowOrDiagIsMatch(spaces[0], spaces[4], spaces[8]);
-                rowOrDiagIsMatch(spaces[2], spaces[4], spaces[6]);
-                console.log(isMatch);
-                return isMatch;
-            }
-            const play = function() {
-                echo();
-                let round = 0;
-                let gameOver = false;
-                while (!gameOver && round < 9) {
-                    let choice = prompt("Enter the board index number to make a move");
-                    let currentPlayer = players[!flip ^ (round % 2)]; //returns whose turn it is on the given round based on the initial flip
-                    makeMove(currentPlayer,choice);
-                    gameOver = checkForWin();
-                    round ++;
-                }
-            }
-            const flip = Math.floor(Math.random()*2)
-            const players = makePlayers(); 
-            const gameBoards = makeGameBoard();
-            return {play}
-        }
-        return {create};
-    })();
-    return {logic}
+const GameBoard = (function() {
+    const create = function() {
+        let spaces = ['', '', '', '', '', '', '', '', ''];
+        const checkForWin = function() {
+            let isMatch = false;
+            function rowOrDiagIsMatch (a,b,c) {
+                if (a === b && a === c && a != false) {
+                    isMatch = true;
+                };
+            };
+            rowOrDiagIsMatch(spaces[0], spaces[1], spaces[2]);
+            rowOrDiagIsMatch(spaces[3], spaces[4], spaces[5]);
+            rowOrDiagIsMatch(spaces[6], spaces[7], spaces[8]);
+            rowOrDiagIsMatch(spaces[0], spaces[3], spaces[6]);
+            rowOrDiagIsMatch(spaces[1], spaces[4], spaces[7]);
+            rowOrDiagIsMatch(spaces[2], spaces[5], spaces[8]);
+            rowOrDiagIsMatch(spaces[0], spaces[4], spaces[8]);
+            rowOrDiagIsMatch(spaces[2], spaces[4], spaces[6]);
+            return isMatch;
+        };
+        return {spaces, checkForWin};
+    };
+    return {create};
 })();
 
-const gameOne = game.logic.create();
-gameOne.play();1
+const Player = (function() {
+    const create = function(playerName, flip) {
+        const name = playerName;
+        const symbol = flip ? 'x' : 'o';
+        return {name, symbol};
+    };
+    return {create};
+})();
+
+const playGame = (function() {
+    const flip = Math.floor(Math.random()*2)
+    const players = [Player.create('Chris', flip), Player.create('Erika', !flip)]
+    const gameBoard = GameBoard.create()
+    const makeMove = function() {
+        let currentPlayer = players[flip ^ (i % 2)]; //returns whose turn it is on the given round based on the initial flip
+        let isValidMove = false;
+        while (!isValidMove) {
+            let choice = prompt("Enter the board index number to make a move");
+            if (gameBoard.spaces[choice] == '') {
+                gameBoard.spaces[choice] = currentPlayer.symbol;
+                isValidMove = true;
+            } else {
+                console.log('Choose another space')
+            }
+        }
+    };
+    const init = (function() {
+        let gameOver = false;
+        console.log(players[0], players[1])
+        for (i=1; i<=9 && !gameOver; i++) {
+            makeMove();
+            gameOver = gameBoard.checkForWin();
+            console.log(`round ${i}: ${gameBoard.spaces}`);
+        };
+    })();
+})();
+
+
